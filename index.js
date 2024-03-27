0b3aeb22f1d963810778b3d5282b4615dfc2223d
Barmao Brian <barmaobrian5@gmail.com>
2024-03-27 15:11:03 +0300

    var-lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+//let  companyName ='Scuber';
+const companyName ='Scuber';
+
+//const mostProfitableNeighborhood ='Chelsea';
+let mostProfitableNeighborhood ='Chelsea';
+
+
+//const companyCeo = 'Susan Smith';
+let companyCeo = 'Susan Smith';
+
+
+
+
+
+
+//side_note
+
+const comb = companyCeo+' '+companyName+' '+mostProfitableNeighborhood
+
+console.log(comb)
+
